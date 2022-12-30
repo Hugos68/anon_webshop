9aadd97e319f668b3ae04c4b5fd56b15d2acb885
hugos68 <hugokorteapple@gmail.com>
2022-12-30 16:09:36 +0100

    All providers are in place and working, homescreen has nice landing page

--- client/src/routes/home/+page.svelte
+++ client/src/routes/home/+page.svelte
@@ -8,14 +8,19 @@
         <h1>Subjective.</h1>
         <p in:fly={{x: 200, duration: 1500}}>Where minds <span>aren't</span> alike</p>
     </div>
-    <div>
-        <img>
-    </div>
+    <img in:fly={{y: 100, duration: 1250}} src="slow-down-pose-2.png" alt="slow_down_pose">
 </main>
 
 <style>
+    main {
+        display: flex;
+    }
     h1 {
         font-size: 6rem;
         text-align: left;
+    }
+    img {
+        max-width: 35vw;
+        margin-left: auto;
     }
 </style>
