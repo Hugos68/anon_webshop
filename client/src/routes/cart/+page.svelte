d950578ad5c0e09237fe11ef00e179a5180bf763
hugos68 <hugokorteapple@gmail.com>
2023-01-09 19:18:21 +0100

    Basic shopping cart screen

--- client/src/routes/cart/+page.svelte
+++ client/src/routes/cart/+page.svelte
@@ -1,65 +1,47 @@
 <script lang="ts">
     import {cart} from "$lib/stores/cart";
+    import {flip} from "svelte/animate";
+
+    console.log($cart);
 </script>
 
-<h1>Shopping cart</h1>
-{#each $cart as cartItem}
-    <div class="cartListing">
-        <p class="title">{cartItem.product.title}</p>
-        <div class="quantity-controls">
-            <button class="subtract" on:click={() => cart.removeProduct(cartItem.product)}><svg viewBox="0 0 16 16"><path fill="#000000" fill-rule="evenodd" d="M3.5 7.75A.75.75 0 014.25 7h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75z" clip-rule="evenodd"/></svg></button>
-            <p>{cartItem.quantity}</p>
-            <button class="add" on:click={() => {cart.addProduct(cartItem.product)}}><svg viewBox="0 0 1024 1024"><path d="M576.477 448.471l349.649.621c35.346.041 63.967 28.728 63.926 64.074s-28.728 63.967-64.074 63.926l-349.429-.621.194 349.647c.013 35.346-28.63 64.01-63.977 64.023s-64.01-28.63-64.023-63.977l-.195-349.921-349.622-.621C63.58 575.581 34.959 546.894 35 511.548s28.728-63.967 64.074-63.926l349.402.621-.194-349.361c-.013-35.346 28.63-64.01 63.977-64.023s64.01 28.63 64.023 63.977l.194 349.635z"/></svg></button>
+
+<main class="flex md:flex-col gap-6">
+    <div class="card w-[min(20rem, 25vw)] bg-primary  shadow-xl">
+        <div class="card-body gap-6">
+            <h2 class="card-title">Shopping cart</h2>
+            {#key $cart}
+                {#if cart.quantity() > 0}
+                    {#each $cart as cartItem (cartItem.product.id)}
+                        <div class="flex items-center justify-between" animate:flip={{duration: 250}}>
+                            <button class="btn btn-ghost btn-circle" on:click={cart.removeProduct(cartItem.product)}>
+                                <svg class="w-10 h-10" viewBox="0 0 48 48"><path d="M24,3A21,21,0,1,0,45,24,21,21,0,0,0,24,3Z" fill="#f8bcc4"/><path d="M32,26H16a2,2,0,0,1,0-4H32A2,2,0,0,1,32,26Z" fill="#ee586c"/></svg>
+                            </button>
+                            <div class="card bg-secondary text-accent">
+                                <div class="card-body">
+                                    <h2 class="card-title">{cartItem.product.title}</h2>
+                                    <p>Amount: {cartItem.quantity}</p>
+                                </div>
+                            </div>
+                            <button class="btn btn-ghost btn-circle" on:click={cart.addProduct(cartItem.product)}>
+                                <svg class="w-10 h-10" viewBox="0 0 48 48"><path d="M24,3A21,21,0,1,0,45,24,21,21,0,0,0,24,3Z" fill="#afe0f5"/><path d="M32,26H16a2,2,0,0,1,0-4H32A2,2,0,0,1,32,26Z" fill="#38b1e7"/><path d="M24,34a2,2,0,0,1-2-2V16a2,2,0,0,1,4,0V32A2,2,0,0,1,24,34Z" fill="#38b1e7"/></svg>
+                            </button>
+                        </div>
+                    {/each}
+                {:else}
+                    <h2 class="card-title">You have 0 products</h2>
+                    <a href="/shop" class="btn">Go to shop</a>
+                {/if}
+            {/key}
         </div>
     </div>
-{/each}
-<form action="?/checkout" class="total-checkout-container">
-    {#key $cart}
-        <label>Total: ${cart.price()}</label>
-        <button disabled="{cart.price()===0}">Checkout</button>
-    {/key}
-</form>
-
-
-
-<style>
-    .title {
-        font-size: 1.5rem;
-    }
-    .cartListing {
-        display: flex;
-        gap: 2rem;
-    }
-    .quantity-controls {
-        display: flex;
-        align-items: center;
-        gap: 1rem;
-    }
-    .quantity-controls > button {
-        min-width: unset;
-        width: 1.75rem;
-        height: 1.75rem;
-        border-radius: 50%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        color: black;
-    }
-    .subtract {
-        background-color: #ff9999;
-    }
-    .add {
-        background-color: #99ff99;
-    }
-    .total-checkout-container {
-        display: flex;
-        justify-content: space-between;
-    }
-    .total-checkout-container > button {
-
-    }
-    .total-checkout-container > button:disabled {
-
-    }
-
-</style>
+    <div class="card w-[min(20rem, 25vw)] bg-primary shadow-xl">
+        <div class="card-body">
+            <h2 class="card-title">Checkout</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+                <button class="btn btn-primary">Purchase</button>
+            </div>
+        </div>
+    </div>
+</main>
