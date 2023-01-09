6ac029cfb53b526e9dd1ebe5a07647f9c66f6772
Hugos68 <hugokorteapple@gmail.com>
2023-01-09 11:07:52 +0100

    Cleanup

--- client/src/lib/util.ts
+++ client/src/lib/util.ts
@@ -1,8 +1,8 @@
 import type {Cookies} from "@sveltejs/kit";
 
-export const arePersonalizedCookiesAllowed = (cookies : Cookies) => {
-    const consentCookie : string | undefined = cookies.get("cookieconsent");
+export const arePersonalizedCookiesAllowed = (cookies: Cookies) => {
+    const consentCookie: string | undefined = cookies.get("cookieconsent");
     if (!consentCookie) return false;
-    const consentCookieParsed : ConsentCookie = JSON.parse(consentCookie);
+    const consentCookieParsed: ConsentCookie = JSON.parse(consentCookie);
     return consentCookieParsed.personalized;
 }
